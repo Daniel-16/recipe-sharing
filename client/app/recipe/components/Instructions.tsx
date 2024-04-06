8c490815ce76895811b8f7704608b1ea70a94986
Daniel-16 <danieloloruntoba681@gmail.com>
2024-04-06 15:59:24 +0100

    feat(client): single recipe

--- client/app/recipe/components/Instructions.tsx
+++ client/app/recipe/components/Instructions.tsx
@@ -1,46 +1,25 @@
-export default function Instructions() {
+import { v4 as uuidv4 } from "uuid";
+
+export default function Instructions({ instructions }: any) {
   return (
     <>
       <div className="pt-10">
         <h1 className="text-4xl font-extrabold text-gray-800">Instructions:</h1>
         <ul className="py-5 text-gray-600 space-y-5">
-          <li>
-            <div className="inline-flex items-start justify-items-center space-x-2">
-              <div className="rounded-md py-1 px-2 bg-[#7e525f]">
-                <p className="text-white text-sm font-extrabold">1</p>
-              </div>
-              <p>
-                Lorem ipsum dolor sit amet consectetur, adipisicing elit.
-                Possimus qui aut, deleniti ipsa minus officia.
-              </p>
-            </div>
-          </li>
-          <li>
-            <div className="inline-flex items-start justify-items-center space-x-2">
-              <div className="rounded-md py-1 px-2 bg-[#7e525f]">
-                <p className="text-white text-sm font-extrabold">2</p>
-              </div>
-              <p>
-                Lorem ipsum dolor sit amet consectetur, adipisicing elit.
-                Possimus qui aut, deleniti ipsa minus officia.
-              </p>
-            </div>
-          </li>
-          <li>
-            <div className="inline-flex items-start justify-items-center space-x-2">
-              <div className="rounded-md py-1 px-2 bg-[#7e525f]">
-                <p className="text-white text-sm font-extrabold">3</p>
-              </div>
-              <p>
-                Lorem ipsum dolor sit amet consectetur, adipisicing elit.
-                Possimus qui aut, deleniti ipsa minus officia. Lorem ipsum dolor
-                sit amet consectetur adipisicing elit. Illo, numquam. Lorem
-                ipsum dolor, sit amet consectetur adipisicing elit. Nostrum,
-                eveniet. Lorem ipsum dolor, sit amet consectetur adipisicing
-                elit. Quis, excepturi.
-              </p>
-            </div>
-          </li>
+          {instructions?.map((instruction: any, index: number) => {
+            return (
+              <li key={uuidv4()}>
+                <div className="inline-flex items-start justify-items-center space-x-2">
+                  <div className="rounded-md py-1 px-2 bg-[#7e525f]">
+                    <p className="text-white text-sm font-extrabold">
+                      {index + 1}
+                    </p>
+                  </div>
+                  <p>{instruction}</p>
+                </div>
+              </li>
+            );
+          })}
         </ul>
       </div>
     </>
--- client/app/recipe/components/RecipeDetails.tsx
+++ client/app/recipe/components/RecipeDetails.tsx
@@ -1,18 +1,28 @@
 import Image from "next/image";
-import foodImage from "../../../assets/michele-blackwell-rAyCBQTH7ws-unsplash.jpg";
 import Ingredients from "./Ingredients";
 import Instructions from "./Instructions";
 
-export default function RecipeDetails() {
+export default function RecipeDetails({
+  imageUrl,
+  timeFrame,
+  description,
+  ingredients,
+  instructions,
+  upvotes,
+}: any) {
   return (
     <div>
       <div className="border px-10 mt-5 mb-5"></div>
-      <Image
-        src={foodImage}
-        alt="Food"
-        className="w-auto h-[60vh] mx-auto object-contain"
-        priority
-      />
+      {imageUrl && (
+        <Image
+          src={imageUrl}
+          alt="Food"
+          className="w-auto h-[60vh] mx-auto object-contain"
+          priority
+          width={100}
+          height={100}
+        />
+      )}
       <div className="w-[20vh]">
         <div className="border border-gray-300 items-center flex py-1 px-2 rounded-lg mt-3 hover:bg-gray-100 hover:cursor-pointer">
           <div className="inline-flex items-center space-x-2">
@@ -30,24 +40,28 @@ export default function RecipeDetails() {
                 fill="#7e525f"
               ></path>
             </svg>
-            <p className="text-gray-800 text-sm font-semibold">100 votes</p>
+            <p className="text-gray-800 text-sm font-semibold">
+              {upvotes.length} votes
+            </p>
           </div>
         </div>
       </div>
       <div className="py-5 inline-flex items-center justify-center space-x-2">
         <p className="text-gray-800 text-2xl font-extrabold">Cooking time:</p>
-        <span className="text-gray-600 text-2xl font-extrabold">10 mins</span>
+        <span className="text-gray-600 text-2xl font-extrabold">
+          {timeFrame?.hours < 1
+            ? `${timeFrame?.minutes} min`
+            : timeFrame?.hours === 1
+            ? `${timeFrame?.hours} hour, ${timeFrame?.minutes} min`
+            : `${timeFrame?.hours} hours, ${timeFrame?.minutes} min`}
+        </span>
       </div>
-      <p className="text-gray-600">
-        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Laboriosam
-        similique sequi doloribus, vero quos ratione ex asperiores nostrum.
-        Voluptates, sit.
-      </p>
+      <p className="text-gray-600">{description}</p>
       <div className="pt-10">
         <h1 className="text-4xl font-extrabold text-gray-800">Ingredients:</h1>
-        <Ingredients />
+        <Ingredients ingredients={ingredients} />
       </div>
-      <Instructions />
+      <Instructions instructions={instructions} />
     </div>
   );
 }
