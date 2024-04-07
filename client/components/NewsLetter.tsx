b2750deb94b8d3b4cf95cfe26f37e25a8ba6476a
Daniel-16 <danieloloruntoba681@gmail.com>
2024-04-07 19:58:28 +0100

    fix: footer

--- client/components/NewsLetter.tsx
+++ client/components/NewsLetter.tsx
@@ -69,16 +69,16 @@ export default function NewsLetter() {
       className="bg-[#FFF0ED] w-full py-20 mt-10 mx-auto text-center px-4"
     >
       <div className="">
-        <h1 className="text-4xl text-gray-800 font-extrabold">
+        <h1 className="text-2xl md:text-4xl text-gray-800 font-extrabold">
           Let&apos;s stay in touch
         </h1>
-        <p className="text-3xl text-gray-600 pt-5 font-medium">
+        <p className="text-xl md:text-3xl text-gray-600 pt-5 font-medium">
           You can join our newsletter, so that we reach out to you with
           <br /> our news and offers.
         </p>
       </div>
       <form onSubmit={handleSubmit}>
-        <div className="md:flex md:items-center md:gap-4 px-10 mt-5 md:justify-center grid space-y-4 md:space-y-0">
+        <div className="justify-center md:flex md:items-center md:gap-4 px-10 mt-5 md:justify-center grid space-y-4 md:space-y-0">
           <input
             type="email"
             required
